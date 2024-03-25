113419f4545d9de6a33f3593586a7bac5d53f530
FirasChalbi <firaschalbi0@gmail.com>
2024-03-25 17:03:51 +0100

    fix menu & footer

--- component/FirstSection.jsx
+++ component/FirstSection.jsx
@@ -29,17 +29,17 @@ function FirstSection() {
       <video className={`video-slide ${activeSlide === 4 ? 'active' : ''}`} src="/videos/bvideo.mp4" autoPlay muted loop></video> */}
 
       <div className={`content ${activeSlide === 0 ? 'active' : ''}`}>
-        <h1>Wonderful.<br /><span>Island</span></h1>
+        <h1>Camping.<br /><span>Enjoy</span></h1>
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate reprehenderit in voluptate reprehenderit in voluptate.</p>
         <Button />
       </div>
       <div className={`content ${activeSlide === 1 ? 'active' : ''}`}>
-        <h1>Camping.<br /><span>Enjoy</span></h1>
+      <h1>Wonder.<br /><span>Island</span></h1>
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate reprehenderit in voluptate reprehenderit in voluptate.</p>
         <a href="#">Read More</a>
       </div>
       <div className={`content ${activeSlide === 2 ? 'active' : ''}`}>
-      <h1>Wonderful.<br /><span>Island</span></h1>
+      <h1>Wonder.<br /><span>Island</span></h1>
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
         <a href="#">Read More</a>
       </div>
