26f25aaa4200da6999ee4e2828340fc0b0588fda
FirasChalbi <firaschalbi0@gmail.com>
2024-08-03 19:20:00 +0200

    fix Server Components

--- app/product/[category]/[name]/page.jsx
+++ app/product/[category]/[name]/page.jsx
@@ -4,7 +4,7 @@ import SingleProduct from "./SingleProduct";
 
 async function page({ params }) {
   const { name, category } = params;
-  const data = await axios.get(`http://localhost:3000/api/products/${category}/${name}`);
+  const data = await axios.get(`https://bloomingagriafrica.tn/api/products/${category}/${name}`);
   return (
     <div>
       <SingleProduct product={data.data}/>;
